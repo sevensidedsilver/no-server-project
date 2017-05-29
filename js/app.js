ce68284080a49189fd9b5082959707f4ef4883a3
Sean <sevensidedsilver@gmail.com>
2017-05-29 11:19:13 -0600

    getting to pages again

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ angular.module('app', ['ui.router'])
         $stateProvider
             .state('home',{
                 url:'/',
-                templateUrl: "../templates/home.html"
+                templateUrl: "no-server-project/templates/home.html"
             })
 
             .state('litehome',{
